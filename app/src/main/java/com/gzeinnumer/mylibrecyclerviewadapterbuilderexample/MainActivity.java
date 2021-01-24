ba4243d31bbd0b2490f3f2d4c5721247b9050704
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-01-24 16:08:39 +0700

    readme

--- app/src/main/java/com/gzeinnumer/mylibrecyclerviewadapterbuilderexample/MainActivity.java
+++ app/src/main/java/com/gzeinnumer/mylibrecyclerviewadapterbuilderexample/MainActivity.java
@@ -2,6 +2,8 @@ package com.gzeinnumer.mylibrecyclerviewadapterbuilderexample;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Toast;
 
@@ -10,11 +12,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.gzeinnumer.mylibrecyclerviewadapterbuilder.AdapterBuilder;
 import com.gzeinnumer.mylibrecyclerviewadapterbuilder.AdapterCreator;
-import com.gzeinnumer.mylibrecyclerviewadapterbuilder.BindViewHolder;
+import com.gzeinnumer.mylibrecyclerviewadapterbuilder.helper.BindViewHolder;
+import com.gzeinnumer.mylibrecyclerviewadapterbuilder.helper.FilterCallBack;
 import com.gzeinnumer.mylibrecyclerviewadapterbuilderexample.databinding.ActivityMainBinding;
 import com.gzeinnumer.mylibrecyclerviewadapterbuilderexample.databinding.RvItemBinding;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,21 +40,44 @@ public class MainActivity extends AppCompatActivity {
             list.add(new MyModel(i, "Data Ke " + (i + 1)));
         }
         AdapterCreator<MyModel> adapter = new AdapterBuilder<MyModel>(R.layout.rv_item)
+                .setList(list)
                 .setCustomNoItem(R.layout.custom_empty_item)
                 .setAnimation(R.anim.anim_two)
-                .setList(list)
-                .onBind(new BindViewHolder() {
+                .setDivider(R.layout.custom_divider)
+                .onBind(new BindViewHolder<MyModel>() {
                     @Override
-                    public void bind(View holder, int position) {
-                        //rv_item -> RvItemBinding
+                    public void bind(View holder, MyModel data, int position) {
                         RvItemBinding bindingItem = RvItemBinding.bind(holder);
-                        bindingItem.btn.setText(list.get(position).id + "_" + list.get(position).name);
+                        bindingItem.btn.setText(data.getId() + "_" + data.getName());
                         bindingItem.btn.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
                                 Toast.makeText(MainActivity.this, "tekan " + position, Toast.LENGTH_SHORT).show();
                             }
                         });
+                    }
+                })
+                .onFilter(new FilterCallBack<MyModel>() {
+                    @Override
+                    public List<MyModel> performFiltering(CharSequence constraint, List<MyModel> listFilter) {
+                        List<MyModel> fildteredList = new ArrayList<>();
+                        if (constraint == null || constraint.length() == 0) {
+                            Collections.sort(listFilter, new Comparator<MyModel>() {
+                                @Override
+                                public int compare(MyModel o1, MyModel o2) {
+                                    return o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase());
+                                }
+                            });
+                            fildteredList.addAll(listFilter);
+                        } else {
+                            String filterPattern = constraint.toString().toLowerCase().trim();
+                            for (MyModel item : listFilter) {
+                                if (String.valueOf(item.getId()).toLowerCase().contains(filterPattern)) {
+                                    fildteredList.add(item);
+                                }
+                            }
+                        }
+                        return fildteredList;
                     }
                 });
 
@@ -70,5 +98,21 @@ public class MainActivity extends AppCompatActivity {
             }
         }.start();
 
+        //use filter on TextWacher
+        binding.ed.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //call the filter
+                adapter.getFilter().filter(s);
+            }
+        });
     }
 }
