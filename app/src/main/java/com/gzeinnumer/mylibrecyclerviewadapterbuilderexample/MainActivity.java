e9413464abeaf90471cc843fa8b19c8f6cac307d
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-01-24 11:26:49 +0700

    readme

--- app/src/main/java/com/gzeinnumer/mylibrecyclerviewadapterbuilderexample/MainActivity.java
+++ app/src/main/java/com/gzeinnumer/mylibrecyclerviewadapterbuilderexample/MainActivity.java
@@ -1,14 +1,73 @@
 package com.gzeinnumer.mylibrecyclerviewadapterbuilderexample;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.gzeinnumer.mylibrecyclerviewadapterbuilder.AdapterBuilder;
+import com.gzeinnumer.mylibrecyclerviewadapterbuilder.AdapterCreator;
+import com.gzeinnumer.mylibrecyclerviewadapterbuilder.BindViewHolder;
+import com.gzeinnumer.mylibrecyclerviewadapterbuilderexample.databinding.ActivityMainBinding;
+import com.gzeinnumer.mylibrecyclerviewadapterbuilderexample.databinding.RvItemBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initAdapter();
+    }
+
+    private void initAdapter() {
+        List<MyModel> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(new MyModel(i, "Data Ke " + (i + 1)));
+        }
+        AdapterCreator<MyModel> adapter = new AdapterBuilder<MyModel>(R.layout.rv_item)
+                .setCustomNoItem(R.layout.custom_empty_item)
+                .setAnimation(R.anim.anim_two)
+//                .setList(list)
+                .onBind(new BindViewHolder() {
+                    @Override
+                    public void bind(View holder, int position) {
+                        RvItemBinding bindingItem = RvItemBinding.bind(holder);
+                        bindingItem.btn.setText(list.get(position).id + "_" + list.get(position).name);
+                        bindingItem.btn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MainActivity.this, "tekan " + position, Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+
+        binding.rv.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        binding.rv.hasFixedSize();
+        binding.rv.setAdapter(adapter);
+
+        //after 5 second, new data will appear
+        new CountDownTimer(5000, 1000) {
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+                for (int i = 10; i < 100; i++) {
+                    list.add(new MyModel(i, "Data Ke " + (i + 1)));
+                }
+//                adapter.setList(list);
+            }
+        }.start();
+
     }
 }
